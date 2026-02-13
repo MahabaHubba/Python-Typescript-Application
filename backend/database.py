fd1afd91b9355f61ebb53a523ae669d4f65f6f94
Md Mohibul Haque <150589335+MahabaHubba@users.noreply.github.com>
2026-02-13 12:08:27 +1100

    Securing DB connection
    Provided comments on each section and purpose

--- backend/database.py
+++ backend/database.py
@@ -4,3 +4,28 @@ from sqlalchemy import create_engine
 # Session maker is another standardied tool that allows you to have the same configuration parameters
 # Declarative base connects the Object orientated structure with the relational database.
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# Make a declaration of the db url we are going to connect with.
+DATABASE_URL = "sqlite:///./app.db"
+
+# connect args allows for the connection with the database. You can also customize the way you 
+# connect to the database..
+# the check same thread allows for the database to handle multi threading
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# SessionLocal creates a session factory called SessionLocal
+# Sessionmaker is a function that create a configured factory for Session's
+# objects, essentially a blueprint for making a new session
+# Autocommit false means changes arent automatically committed after each operation
+# Thus you must recall session.commit to save them permanently to the database
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+# Autoflush flalse is a key setting, disabling automatically sending db changes
+# you can control the data being sent to call through session.flush() or session.commit()
+# bind-enginge links the session factory to a specific SQL Alchemy Enginge - Asking what database to talk to.
+
+# Creates a base class for defining the database using Python class.
+Base = declarative_base()
